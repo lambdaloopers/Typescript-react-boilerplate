56c704f595d7b38324a110b9afe002ec4e872711
nhal <originhality@hotmail.com>
2017-12-11 10:38:37 +0100

    added the required files and dependencies for the storybook and tried to translate them to tsx, found some compilation troubles pending to be fixed

---
+++ app/stories/index.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+storiesOf("TypeScript and Storybook", module);
+    // .add('Sample Widget', () => <SampleWidget name="Michael" />);
